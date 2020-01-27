ebdc405b5d87cb17d8cb0a29b72549b898207d06
CaddyDz <caddydz4@gmail.com>
2020-01-28 00:54:10 +0100

    seed services :zzz:
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ database/seeds/ServicesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Service;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Storage::disk('public')->deleteDirectory('services');
+        Storage::disk('public')->makeDirectory('services');
+        factory(Service::class, 50)->create();
+    }
+}
