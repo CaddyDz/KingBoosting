b6ce832ec3386887a57328b13469f78e45ab062c
CaddyDz <caddydz4@gmail.com>
2020-02-01 03:25:54 +0100

    fixed typo and better slider maths
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- resources/js/components/ServicesSlider.vue
+++ resources/js/components/ServicesSlider.vue
@@ -4,17 +4,17 @@
 			<v-sheet min-width="100%" color="transparent" tile>
 				<v-slide-group v-model="model" class="pa-4" center-active mandatory @click="magic">
 					<!-- This is a hack to center the first element -->
-					<v-slider-item v-for="n in 4" :key="n">
-						<v-card color="transparent" height="210" class="ma-4" width="150" :ripple="false">
+					<v-slide-item v-for="n in 3" :key="n">
+						<v-card color="transparent" height="210" class="ma-4" width="150" outlined :ripple="false">
 							<v-row>
 								<v-card-text></v-card-text>
 							</v-row>
 						</v-card>
-					</v-slider-item>
+					</v-slide-item>
 					<!-- End hack -->
 					<v-slide-item
 						v-for="(service, index) in services"
-						:key="index"
+						:key="index + 4"
 						v-slot:default="{ active, toggle }"
 					>
 						<v-card
@@ -24,7 +24,9 @@
 							class="ma-4"
 							width="150"
 							@click="visit(service.slug), toggle"
+							outlined
 							:ripple="false"
+							hover
 						>
 							<v-row>
 								<img :src="service.image" :alt="service.name" />
@@ -49,25 +51,24 @@ export default {
 		}
 	},
 	data: () => ({
-		model: 1,
+		model: null,
 		services: []
 	}),
 	methods: {
 		visit(slug) {
 			window.location = "/" + slug;
-		},
-		magic() {
-			//
 		}
+	},
+	created() {
+		// Get the window location slug
+		// Get the id of the service from that slug
+		// Set the data model to that id so that the selected service is centered
+		this.model = this.service.id + 2;
 	},
 	mounted() {
 		axios
 			.get("/api/services")
 			.then(response => (this.services = response.data));
-		this.model = this.service.id - 1;
-		// Get the window location slug
-		// Get the id of the service from that slug
-		// Set the data model to that id so that the selected service is centered
 	}
 };
 </script>
@@ -77,4 +78,8 @@ export default {
 	background-image: url("/img/service_nav_bg.png");
 	background-position: center bottom;
 }
+</style>
+
+<style lang="scss">
+$card-border-radius: 50% !default;
 </style>
