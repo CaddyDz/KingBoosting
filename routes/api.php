395c50739caec1f9f2004e3fed83fcf891af1670
CaddyDz <caddydz4@gmail.com>
2020-02-03 20:34:59 +0100

    Return tiers with their divisions from API
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,6 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::post('/tiers', 'TiersController@index');
 Route::get('/services', 'ServicesController@index');
 Route::get('/services/{id}/kind', 'ServicesController@getKind');
