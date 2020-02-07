08ef90eeea167ead8e356c01aadad6a0fafbe452
CaddyDz <caddydz4@gmail.com>
2020-02-07 13:05:07 +0100

    Removed axios default header
    Triggers a preflight which causes CORS policy blocking requests
    
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,8 +6,8 @@ window._ = require('lodash');
  */
 
 window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// Triggers a preflight which causes CORS policy blocking requests
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
