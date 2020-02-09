9630ade8baf17128dc950ce54c3fdbb3119b8ae2
CaddyDz <caddydz4@gmail.com>
2020-02-09 19:24:25 +0100

    Refactored roles and permissions seeder
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- database/seeds/RolesAndPermissionsTablesSeeder.php
+++ database/seeds/RolesAndPermissionsTablesSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
 
 class RolesAndPermissionsTablesSeeder extends Seeder
 {
@@ -13,12 +14,35 @@ class RolesAndPermissionsTablesSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'Super Admin']); // All Permissions Granted implicitly
-        $member = Role::create(['name' => 'Member']);
-        $booster = Role::create(['name' => 'Booster']);
-        $moderator = Role::create(['name' => 'Moderator']);
-        $accessMembersArea = Permission::create(['name' => 'Access Members Area']);
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit orders']);
+        Permission::create(['name' => 'delete orders']);
+        Permission::create(['name' => 'lock orders']);
+        Permission::create(['name' => 'unlock orders']);
+        // Every logged in user can access members area so this is redundant for now
+        Permission::create(['name' => 'Access Members Area']);
+
+        // create roles and assign created permissions
+
+        // this can be done as separate statements
+        $role = Role::create(['name' => 'Member']);
+        $role->givePermissionTo('edit orders', 'Access Members Area');
+
+        // or may be done by chaining
+        $role = Role::create(['name' => 'Moderator'])
+            ->givePermissionTo(['lock orders', 'unlock orders', 'Access Members Area']);
+
+        // or may be done by chaining
+        $role = Role::create(['name' => 'Booster'])
+            ->givePermissionTo(['lock orders', 'unlock orders', 'Access Members Area']);
+
+        $role = Role::create(['name' => 'Super Admin']);
+        // All Permissions Granted implicitly
+        $role->givePermissionTo(Permission::all());
+
         // Anyone can place an order without even an account ¯\_(ツ)_/¯
-        $member->givePermissionTo($accessMembersArea);
     }
 }
