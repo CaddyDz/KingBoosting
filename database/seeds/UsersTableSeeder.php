db8d2f5750ee48d1f325890b6160866d169ff498
CaddyDz <caddydz4@gmail.com>
2020-02-09 19:23:28 +0100

    Seeded 50 members, 25 boosters & 5 moderators
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,5 +19,17 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('*iUJA2m%Ey67')
         ]);
         // Grant Super Admin role to god
+        // Create 50 members
+        factory(User::class, 50)->create()->each(function ($user) {
+            $user->assignRole('Member');
+        });
+        // Create 25 boosters
+        factory(User::class, 25)->create()->each(function ($user) {
+            $user->assignRole('Booster');
+        });
+        // Create 5 moderators
+        factory(User::class, 5)->create()->each(function ($user) {
+            $user->assignRole('Moderator');
+        });
     }
 }
