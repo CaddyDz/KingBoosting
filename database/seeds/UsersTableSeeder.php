20608b648e16fcfc8ab9c4320d8b0424aae04828
CaddyDz <caddydz4@gmail.com>
2020-02-20 18:43:26 +0100

    Seed orders for boosters as if they're clients
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -25,9 +25,14 @@ class UsersTableSeeder extends Seeder
         // We need to seed boosters first because they handle orders
         // Boosters don't create orders, except they do
         factory(User::class, 25)->create()->each(function ($booster) use ($admin) {
-            // Admin has 25 orders
-            $admin->orders()->create(factory(Order::class)->make(['booster_id' => $booster->id])->toArray());
+            // Assign the booster role
             $booster->assignRole('Booster');
+            // Create orders for the admin and assign them to the current booster
+            $admin->orders()->create(factory(Order::class)->make(['booster_id' => $booster->id])->toArray());
+            // Get a random booster from DB, create a random number of orders for them, and assign them to the current booster
+            $dbBooster = User::role('Booster')->inRandomOrder()->limit(1)->first();
+            if ($dbBooster) $dbBooster->orders()->createMany(factory(Order::class, rand(3, 20))->make(['booster_id' => $booster->id])->toArray());
+            // Admin has 25 orders
             // Create 50 members
             factory(User::class, 2)->create()->each(function ($client) use ($booster) {
                 $client->assignRole('Member');
@@ -40,10 +45,5 @@ class UsersTableSeeder extends Seeder
                 $user->orders()->createMany(factory(Order::class, rand(3, 20))->make(['booster_id' => $booster->id])->toArray());
             });
         });
-        // 25 boosters exchange orders
-        // factory(User::class, 25)->create()->each(function ($user) {
-        //     $user->assignRole('Booster');
-        //     $user->jobs()->createMany(factory(Order::class, rand(3, 20))->make()->toArray());
-        // });
     }
 }
