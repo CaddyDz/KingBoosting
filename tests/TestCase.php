52bc5ac1d2e6fa67765cc15e928cfc26c80b18e8
CaddyDz <caddydz4@gmail.com>
2020-02-28 01:08:41 +0100

    Global function to login in tests
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,15 +2,37 @@
 
 namespace Tests;
 
+use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use DatabaseMigrations;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->withoutExceptionHandling();
+    }
+
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param Illuminate\Contracts\Auth\Authenticatable
+     *
+     * @return App\User authenticated
+     */
+    protected function login(string $role, ?User $user = null): TestCase
+    {
+        $role = Role::create(['name' => $role]);
+        $role->givePermissionTo(Permission::all());
+        $user = $user ?: create(User::class);
+        $user->assignRole($role);
+        $this->be($user);
+        return $this;
     }
 }
