011163b762e13708ae4999091289d0952812f799
CaddyDz <caddydz4@gmail.com>
2020-02-29 21:06:22 +0100

    Corrected Formatting
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- frontend/components/WhyUs.vue
+++ frontend/components/WhyUs.vue
@@ -1,40 +1,44 @@
 <template>
 	<div class="why-us">
 		<div class="section-name">
-			<h1>
-			Why Us
-			</h1>
-      	</div>
+			<h1>Why Us</h1>
+		</div>
 		<v-container class="container">
-			<v-row >
+			<v-row>
 				<v-card class="card" align-self="center" v-for="(item ,index) in whyUsData" :key="index">
-					<v-col  >
+					<v-col>
 						<v-icon size="100px" class="icons">{{icons[index]}}</v-icon>
 						<v-card-title>
 							<h2>{{ item.title }}</h2>
 						</v-card-title>
 						<v-card-text class="headline">
 							<p>{{ item.description }}</p>
-						</v-card-text>								
+						</v-card-text>
 					</v-col>
 				</v-card>
 			</v-row>
 		</v-container>
-				
 	</div>
 </template>
 
 <script>
 export default {
-	data: () => ({
-		whyUsData: [],
-		icons: ["mdi-school","mdi-google-circles-communities","mdi-chart-bubble","mdi-lock-open-outline"]
-	}),
+	data() {
+		return {
+			whyUsData: [],
+			icons: [
+				"mdi-school",
+				"mdi-google-circles-communities",
+				"mdi-chart-bubble",
+				"mdi-lock-open-outline"
+			]
+		};
+	},
 	methods: {
 		fetchWhyUsData() {
-			this.$axios.$get("/priorities")
-				.then(Response => {this.whyUsData = Response;
-				});
+			this.$axios.$get("/priorities").then(Response => {
+				this.whyUsData = Response;
+			});
 		}
 	},
 	mounted() {
@@ -44,20 +48,20 @@ export default {
 </script>
 
 <style scoped>
-.container{
-	margin-top: 50px; 
+.container {
+	margin-top: 50px;
 	margin-bottom: 30px;
 }
-.icons{
+.icons {
 	background: -webkit-linear-gradient(#673ab7, #9c27b0);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+	background-clip: text;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
 }
 .why-us {
 	background-color: #212224;
 	position: relative;
-	background-image: url('/img/backgrounds/priorities-bg.webp');
+	background-image: url("/img/backgrounds/priorities-bg.webp");
 	-webkit-background-size: cover;
 	-moz-background-size: cover;
 	-o-background-size: cover;
@@ -81,21 +85,21 @@ p {
 	color: #999a9c;
 	font-size: 14px;
 }
-.section-name{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 200px;
-    height: 60px;
-    background: linear-gradient(#673ab7, #9c27b0);
-    z-index: 1; 
-    position: absolute ;
-    top: -30px;
-  }
+.section-name {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 200px;
+	height: 60px;
+	background: linear-gradient(#673ab7, #9c27b0);
+	z-index: 1;
+	position: absolute;
+	top: -30px;
+}
 
-  .card {
-	  margin-top: 20px;
-	  margin-bottom: 20px;
-	  opacity: .8;
-  }
+.card {
+	margin-top: 20px;
+	margin-bottom: 20px;
+	opacity: 0.8;
+}
 </style>
