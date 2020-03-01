0a27ae97f180f2ff95c02dd5a83e99faffa484ec
CaddyDz <caddydz4@gmail.com>
2020-03-01 18:24:28 +0100

    Seed order purchase for duoq & regular boosting
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -9,14 +9,15 @@ use Faker\Generator as Faker;
 $factory->define(Order::class, function (Faker $faker) {
     $service_id = rand(1, 14);
     $purchase = function ($service_id) {
+        $from_tier = Tier::find(rand(1, 3));
+        $to_tier = Tier::find(rand(4, 6));
+        $from_division = $from_tier->divisions[rand(0, 3)]->name;
+        $to_division = $to_tier->divisions[rand(0, 3)]->name;
         switch ($service_id['service_id']) {
             case 1:
-                return "14 wins";
+                return "14 wins in $from_tier->name $from_division";
             case 2:
-                $from_tier = Tier::find(rand(1, 3));
-                $to_tier = Tier::find(rand(4, 6));
-                $from_division = $from_tier->divisions[rand(0, 3)]->name;
-                $to_division = $to_tier->divisions[rand(0, 3)]->name;
+            case 4:
                 return "$from_tier->name $from_division to $to_tier->name $to_division";
             default:
                 return "Nothing";
