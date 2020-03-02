84dd148bcea34a781e9b99083e414d654a3ab669
KitaniIslam <slmkitani@gmail.com>
2020-03-02 03:27:05 +0100

    HowToBuy Component design and implementation

---
+++ frontend/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-layout column >
+        <services-circle />
+        <features />
+        <customer-voice />
+        <how-to-buy />
+        <WhyUs />
+        <Blogs />
+    </v-layout>
+</template>
+
+<script>
+import CustomerVoice from '~/components/CustomerVoice.vue';
+import ServicesCircle from '~/components/ServicesCircle.vue';
+import WhyUs from '~/components/WhyUs.vue';
+import Blogs from '~/components/Blogs.vue';
+import Features from '~/components/Features.vue';
+import HowToBuy from '~/components/HowToBuy.vue';
+
+export default {
+    components:{
+        WhyUs,
+        Blogs,
+        CustomerVoice,
+        ServicesCircle,
+        Features,
+        HowToBuy
+    }
+}
+</script>
