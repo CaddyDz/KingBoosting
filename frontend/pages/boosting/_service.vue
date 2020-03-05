fe2eb3ea39a234a35f5dc484cdd77a80ec27b336
KitaniIslam <slmkitani@gmail.com>
2020-03-05 17:26:04 +0100

    boosting component (not complete )

---
+++ frontend/pages/boosting/_service.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1> #{{service}}</h1>
+    <stepper />
+  </div>
+</template>
+
+<script>
+import Stepper from '~/components/Stepper' 
+export default {
+    components:{
+        Stepper
+    },
+    data(){
+      return {
+        service: ""
+      }
+    },
+    mounted(){
+      this.service = this.$attrs.service
+    }
+}
+</script>
+
+<style>
+
+</style>
