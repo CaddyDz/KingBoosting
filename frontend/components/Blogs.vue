16f22f9da9c37ce9d32a0a96e372cab129bc2b82
KitaniIslam <slmkitani@gmail.com>
2020-03-05 21:56:15 +0100

    delete undeclared component from blogs component

--- frontend/components/Blogs.vue
+++ frontend/components/Blogs.vue
@@ -6,7 +6,7 @@
 		<v-row no-gutters>
 			<v-col class="description-col" align-self="center">
 				<h1 class="headline mb-1">
-					<current-blog-number> #{{ article.id }} </current-blog-number>
+					#{{ article.id }}
 					{{ article.title }}
 				</h1>
 				<p class="description-txt">{{ article.excerpt }}</p>
