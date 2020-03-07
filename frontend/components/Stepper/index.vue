8a95ebb978ed400f60d350bc1b0d01e26a4fef1c
CaddyDz <caddydz4@gmail.com>
2020-03-07 19:17:29 +0100

    Formatting
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

--- frontend/components/Stepper/index.vue
+++ frontend/components/Stepper/index.vue
@@ -1,29 +1,23 @@
 <template>
-    <div>
-        <selector :step="steps[0]"/>
-        <selector :step="steps[1]"/>
-        <slider :step="steps[2]"/>
-        <options :step="steps[3]"/>
-    </div>
+	<div>
+		<selector :step="steps[0]" />
+		<slider :step="steps[2]" />
+		<options :step="steps[3]" />
+	</div>
 </template>
 
 <script>
-
 export default {
-    props:["service"],
-    data:() => {
-        return {
-            steps:[
-                {id: 1 , title: "Select Your Current League" },
-                {id: 2 , title: "Select Your Number Of Wins" },
-                {id: 2 , title: "Select Your Number Of Wins" },
-                {id: 3 , title: "Checkout" }
-            ]
-        }
-    }
-}
+	props: ["service"],
+	data() {
+		return {
+			steps: [
+				{ id: 1, title: "Select Your Current League" },
+				{ id: 2, title: "Select Your Number Of Wins" },
+				{ id: 2, title: "Select Your Number Of Wins" },
+				{ id: 3, title: "Checkout" }
+			]
+		};
+	}
+};
 </script>
-
-<style>
-
-</style>
