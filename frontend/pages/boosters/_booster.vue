7a8b363bdf4c69e2012057db193e3778997164c3
CaddyDz <caddydz4@gmail.com>
2020-03-12 10:31:59 +0100

    Boosters page
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ frontend/pages/boosters/_booster.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<p>Hey Im booster {{booster}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			booster: ""
+		};
+	},
+	methods: {
+		getBooster(id) {
+			this.booster = id;
+		}
+	},
+	mounted() {
+		this.getBooster(this.$route.params.Booster);
+	}
+};
+</script>
+
+<style scoped>
+</style>
