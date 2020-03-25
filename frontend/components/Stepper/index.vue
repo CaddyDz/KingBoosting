742d4d8fbefdafc0426b7d8d4737bcf800c6ae4e
Riyadh Aissi <red83711@gmail.com>
2020-03-25 22:48:47 +0100

    Translated indexvue

--- frontend/components/Stepper/index.vue
+++ frontend/components/Stepper/index.vue
@@ -12,12 +12,31 @@ export default {
 	data() {
 		return {
 			steps: [
-				{ id: 1, title: "Select Your Current League" },
-				{ id: 2, title: "Select Your Number Of Wins" },
-				{ id: 2, title: "Select Your Number Of Wins" },
-				{ id: 3, title: "Checkout" }
+				{ id: 1, title: this.$i18n.t("Select Your Current League") },
+				{ id: 2, title: this.$i18n.t("Select Your Number Of Wins") },
+				{ id: 2, title: this.$i18n.t("Select Your Number Of Wins") },
+				{ id: 3, title: this.$i18n.t("Checkout") }
 			]
 		};
 	}
 };
 </script>
+<i18n>
+{
+	"en": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+  },
+    "fr": {
+	    "Select Your Current League": "Sélectionnez votre ligue actuelle",
+	    "Select Your Number Of Wins": "Sélectionnez votre nombre de victoires",
+	    "Checkout":"Paiment"
+  },
+    "it": {
+		"Select Your Current League": "Seleziona la tua lega attuale",
+		"Select Your Number Of Wins": "Seleziona il tuo numero di vittorie",
+		"Checkout": "Pagamento"
+  }
+}
+</i18n>
