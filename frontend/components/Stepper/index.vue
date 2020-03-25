627e0e253eaac6e4601587110b45a7156613d92a
Riyadh Aissi <red83711@gmail.com>
2020-03-26 00:05:24 +0100

    translated index.vue

--- frontend/components/Stepper/index.vue
+++ frontend/components/Stepper/index.vue
@@ -37,6 +37,46 @@ export default {
 		"Select Your Current League": "Seleziona la tua lega attuale",
 		"Select Your Number Of Wins": "Seleziona il tuo numero di vittorie",
 		"Checkout": "Pagamento"
-  }
+  },
+    "de": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	},
+	"at": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	},
+	"swiss": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	},
+	"es": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	},
+	"nl": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	},
+	"se": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	},
+	"ne": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	},
+	"dk": {
+		"Select Your Current League": "Select Your Current League",
+		"Select Your Number Of Wins": "Select Your Number Of Wins",
+		"Checkout": "Checkout"
+	}
 }
 </i18n>
