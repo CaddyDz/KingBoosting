2cb07ca6679f2bf2fd4495123d459d9e7142f29f
Salim Djerbouh <caddydz4@gmail.com>
2020-11-21 12:02:18 +0100

    Seed static user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
 	public function run(): void
 	{
 		\App\Models\User::factory(10)->create();
+		\App\Models\User::factory()->create(['email' => 'user@kingboosting.com']);
 	}
 }
