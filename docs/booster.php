5d35d82c0065cd785a62f6e466dee978fcc51604
Salim Djerbouh <caddydz4@gmail.com>
2020-11-22 08:38:27 +0100

    API docs for boosters usernames endpoint

---
+++ docs/booster.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @OA\Get(
+ ** path="/api/getBoostersNames",
+ *   tags={"Booster"},
+ *   summary="Get usernames of all boosters",
+ *   operationId="booster",
+ *
+ *   @OA\Response(
+ *      response=200,
+ *      description="Success",
+ *      @OA\MediaType(
+ *           mediaType="application/json",
+ *      )
+ *   ),
+ *)
+ **/
