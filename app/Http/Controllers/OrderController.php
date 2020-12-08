f584a16e2f75ac67d6fd33bd87abca39641253c5
Salim Djerbouh <caddydz4@gmail.com>
2020-12-08 02:15:20 +0100

    Only notify selected booster if any
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderRequest;
 use Stripe\Charge;
 use App\Models\Order;
 use App\Models\User;
@@ -19,7 +20,7 @@ class OrderController extends Controller
 	 * @param  \Illuminate\Http\Request  $request
 	 * @return \Illuminate\Http\Response
 	 */
-	public function store(Request $request)
+	public function store(OrderRequest $request)
 	{
 		// Set your secret key. Remember to switch to your live secret key in production!
 		// See your keys here: https://dashboard.stripe.com/account/apikeys
@@ -50,8 +51,13 @@ class OrderController extends Controller
 				'options' => $request->options,
 				'price' => $request->price,
 			]);
-			$users = User::role('Booster')->get();
-			Notification::send($users, new OrderPlaced($order));
+			if ($request->booster) {
+				$booster = User::where('username', $request->booster)->firstOrFail();
+				$booster->notify(new OrderPlaced($order));
+			} else {
+				$users = User::role('Booster')->get();
+				Notification::send($users, new OrderPlaced($order));
+			}
 			return response([
 				'message' => __('Your order has been placed'),
 			]);
