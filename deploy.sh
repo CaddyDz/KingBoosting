4005e5d20d5d1fcc224ad85a2c0928bf87a8c4cf
Salim Djerbouh <caddydz4@gmail.com>
2020-12-22 01:08:47 +0100

    Added forge deploy script
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+cd /home/forge/dashboard.eloboost.app
+git pull origin develop
+composer install --optimize-autoloader
+php artisan migrate:fresh --seed --force
+php artisan l5-swagger:generate
+php artisan optimize
+
+( flock -w 10 9 || exit 1
+    echo 'Restarting FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9>/tmp/fpmlock
