bc65191fb3727fe442681c6fc620d03a5bbaa3f8
Salim Djerbouh <caddydz4@gmail.com>
2021-01-04 17:55:50 +0100

    Set order first in sidebar navigation
    * Also added a shopping cart icon
    
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Anaseqal\NovaSidebarIcons\NovaSidebarIcons;
 use Illuminate\Support\Facades\{Gate, Route};
 use App\Http\Controllers\Nova\LoginController;
 use Laravel\Nova\{Nova, NovaApplicationServiceProvider};
+use EricLagarda\NovaLinkResource\NovaLinkResource;
+
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
@@ -102,7 +105,13 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 	 */
 	public function tools(): array
 	{
-		return [];
+		return [
+			new NovaSidebarIcons,
+			(new NovaLinkResource())->name('Profile')
+				->to('/resources/users/' . auth()->id())
+				->icon('<svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24" height="24"><path fill="var(--sidebar-icon)" d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/></svg>'),
+
+		];
 	}
 
 	/**
