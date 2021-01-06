62f9bc50e38ae5f99c6346f8a91346b58f5ff149
Salim Djerbouh <caddydz4@gmail.com>
2021-01-06 10:57:34 +0100

    Order customer country and details as tag pills
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Nova/Order.php
+++ app/Nova/Order.php
@@ -10,9 +10,11 @@ use NovaIcon\Icon;
 use Timothyasp\Badge\Badge;
 use Illuminate\Http\Request;
 use App\Nova\Actions\LockOrder;
+use App\Nova\Actions\OrderDetails;
 use App\Nova\Filters\OrderFilter;
 use AwesomeNova\Cards\FilterCard;
-use Laravel\Nova\Fields\{BelongsTo, ID, KeyValue, Number, Text};
+use Superlatif\NovaTagInput\Tags;
+use Laravel\Nova\Fields\{BelongsTo, ID, KeyValue, Number, Stack, Text};
 
 class Order extends Resource
 {
@@ -22,6 +24,13 @@ class Order extends Resource
 	 * @var string
 	 */
 	public static $model = \App\Models\Order::class;
+
+	/**
+	 * The logical group associated with the resource.
+	 *
+	 * @var string
+	 */
+	public static $group = null;
 
 	/**
 	 * The order in which to show the resource on the sidebar.
@@ -70,46 +79,53 @@ class Order extends Resource
 	public function fields(Request $request)
 	{
 		return [
-			Icon::make('')
-				->icon(
-					fn (): string => $this->icon
-				)->css(fn (): string => [
-					'pending' => 'text-info',
-					'progress' => 'text-warning-dark',
-					'paused' => 'text-black',
-					'completed' => 'text-success',
-					'suspended' => 'text-danger',
-				][$this->status]),
-			Badge::make('Status')
-				->options([
-					'pending' => __('Awaiting for booster'),
-					'progress' => __('In Progress'),
-					'paused' => __('Paused'),
-					'completed' => __('Complete'),
-					'suspended' => __('Suspended'),
-				])
-				->colors([
-					'pending' => '#64cedb',
-					'progress' => '#d68842',
-					'paused' => '#000',
-					'completed' => '#42d6a9',
-					'suspended' => '#ca404d',
-				])->displayUsingLabels()->exceptOnForms()->sortable(),
+			Stack::make('Status', [
+				Icon::make('')
+					->icon(
+						fn (): string => $this->icon
+					)->css(fn (): string => [
+						'pending' => 'text-info',
+						'progress' => 'text-warning-dark',
+						'paused' => 'text-black',
+						'completed' => 'text-success',
+						'suspended' => 'text-danger',
+					][$this->status]),
+				Badge::make('Status')
+					->options([
+						'pending' => __('Awaiting for booster'),
+						'progress' => __('In Progress'),
+						'paused' => __('Paused'),
+						'completed' => __('Complete'),
+						'suspended' => __('Suspended'),
+					])
+					->colors([
+						'pending' => '#64cedb',
+						'progress' => '#d68842',
+						'paused' => '#000',
+						'completed' => '#42d6a9',
+						'suspended' => '#ca404d',
+					])->displayUsingLabels()
+			])->sortable()->exceptOnForms(),
 			ID::make(__('ID'), 'id')->sortable()->hideFromDetail(),
-			Text::make(__('Login name'), 'riot_login'),
-			Text::make(__('Login password'), 'riot_password'),
-			Text::make(__('Summoner name'), 'summoner')->onlyOnDetail(),
-			Text::make(__('Server'), 'server'),
+			Text::make(__('Purchase'), fn (): string => $this->purchase . '</br>' . country_flag($this->client->country) . ' ' . $this->service)->asHtml()->onlyOnIndex(),
+			// Order details in colored pills
+			Tags::make(__("Order details"), fn () => $this->options),
+			Text::make(__('Price'), 'price')
+				->displayUsing(fn ($price) => '$' . $price)
+				->hideFromDetail(),
+			Text::make(__('Login name'), 'riot_login')->hideFromIndex(),
+			Text::make(__('Login password'), 'riot_password')->hideFromIndex(),
+			Text::make(__('Summoner name'), 'summoner')->hideFromIndex(),
+			Text::make(__('Server'), 'server')->hideFromIndex(),
 			// Current rank?
-			ID::make(__('Order ID'), 'id')->sortable()->onlyOnDetail(),
-			Text::make(__('Item'), 'purchase'),
+			ID::make(__('Order ID'), 'id')->sortable()->hideFromIndex(),
+			Text::make(__('Item'), 'purchase')->hideFromIndex(),
 			Text::make(__('Service'), 'service')->hideFromIndex(),
 			// Customer's country => this client country
-			Text::make(__('Customer\'s country'), fn () => $this->client->country)->onlyOnDetail(),
+			Text::make(__('Customer\'s country'), fn () => $this->client->country)->hideFromIndex(),
 			BelongsTo::make(__('Booster'), 'booster', User::class),
-			BelongsTo::make(__('Client'), 'client', User::class),
+			BelongsTo::make(__('Client'), 'client', User::class)->hideFromIndex(),
 			KeyValue::make(__('Order details'), 'options'),
-			Text::make(__('Price'), 'price')->hideFromDetail(),
 		];
 	}
 
