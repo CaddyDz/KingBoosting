961397c8512463731f9e3f8dc65a6400f82784b2
Salim Djerbouh <caddydz4@gmail.com>
2021-01-07 19:12:13 +0100

    Fixed registration
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\{MessagesController, OrderController};
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
 	Route::post('orders', [OrderController::class, 'store']);
 	Route::post('messages/send', [MessagesController::class, 'store']);
 });
+
+Route::post('register', [RegisterController::class, 'register']);
