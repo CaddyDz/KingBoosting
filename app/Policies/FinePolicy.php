b7685488b6f4886cf3f8bcc0bb38133d993a39ae
Salim Djerbouh <caddydz4@gmail.com>
2021-01-07 20:03:49 +0100

    Added authorization policies
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

---
+++ app/Policies/FinePolicy.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\{Fine, User};
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class FinePolicy
+{
+	use HandlesAuthorization;
+
+	/**
+	 * Determine whether the user can view any models.
+	 *
+	 * @param \App\Models\User $user
+	 * @return mixed
+	 */
+	public function viewAny(User $user): bool
+	{
+		return $user->hasRole('Admin');
+	}
+
+	/**
+	 * Determine whether the user can view the model.
+	 *
+	 * @param \App\Models\User $user
+	 * @param \App\Models\Fine $fine
+	 * @return mixed
+	 */
+	public function view(User $user, Fine $fine): bool
+	{
+		return $user->hasRole('Admin');
+	}
+
+	/**
+	 * Determine whether the user can create models.
+	 *
+	 * @param \App\Models\User $user
+	 * @return mixed
+	 */
+	public function create(User $user): bool
+	{
+		return false;
+	}
+
+	/**
+	 * Determine whether the user can update the model.
+	 *
+	 * @param \App\Models\User $user
+	 * @param \App\Models\Fine $fine
+	 * @return mixed
+	 */
+	public function update(User $user, Fine $fine): bool
+	{
+		return false;
+	}
+
+	/**
+	 * Determine whether the user can delete the model.
+	 *
+	 * @param \App\Models\User $user
+	 * @param \App\Models\Fine $fine
+	 * @return mixed
+	 */
+	public function delete(User $user, Fine $fine): bool
+	{
+		return false;
+	}
+
+	/**
+	 * Determine whether the user can restore the model.
+	 *
+	 * @param \App\Models\User $user
+	 * @param \App\Models\Fine $fine
+	 * @return mixed
+	 */
+	public function restore(User $user, Fine $fine): bool
+	{
+		return false;
+	}
+
+	/**
+	 * Determine whether the user can permanently delete the model.
+	 *
+	 * @param \App\Models\User $user
+	 * @param \App\Models\Fine $fine
+	 * @return mixed
+	 */
+	public function forceDelete(User $user, Fine $fine): bool
+	{
+		return false;
+	}
+}
