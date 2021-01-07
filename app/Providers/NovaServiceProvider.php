afe1c997ea216c28dba5b103c6fdbde75c12e874
Salim Djerbouh <caddydz4@gmail.com>
2021-01-07 16:53:55 +0100

    Added custom menu to dashboard sidebar
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -8,9 +8,10 @@ use App\Nova\Dashboards\Rules;
 use Illuminate\Support\Facades\{Gate, Route};
 use App\Http\Controllers\Nova\LoginController;
 use Anaseqal\NovaSidebarIcons\NovaSidebarIcons;
-use EricLagarda\NovaLinkResource\NovaLinkResource;
+use App\Nova\{Application, Booster, Fine, Order, User};
 use Laravel\Nova\{Nova, NovaApplicationServiceProvider};
-
+use DigitalCreative\CollapsibleResourceManager\CollapsibleResourceManager;
+use DigitalCreative\CollapsibleResourceManager\Resources\{InternalLink, NovaResource, TopLevelResource};
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
 {
@@ -111,10 +112,39 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 	{
 		return [
 			new NovaSidebarIcons,
-			(new NovaLinkResource())->name('Profile')
-				->to('/resources/users/' . auth()->id())
-				->icon('<svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24" height="24"><path fill="var(--sidebar-icon)" d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/></svg>'),
-
+			new CollapsibleResourceManager([
+				'navigation' => [
+					TopLevelResource::make([
+						'label' => 'Orders',
+						'icon' => Order::icon(),
+						'linkTo' => Order::class,
+					]),
+					TopLevelResource::make([
+						'label' => 'Profile',
+						'icon' => User::icon(),
+						'linkTo' => NovaResource::make(User::class)->detail(auth()->id()),
+					]),
+					TopLevelResource::make([
+						'label' => 'Rules',
+						'icon' => view('nova::svg.icon-rules')->render(),
+						'linkTo' => InternalLink::make([
+							'target' => '_self',
+							'path' => '/dashboards/rules',
+						]),
+					]),
+					TopLevelResource::make([
+						'label' => 'Admin',
+						'expanded' => false,
+						'icon' => view('nova::svg.icon-dashboard')->render(),
+						'resources' => [
+							User::class,
+							Booster::class,
+							Fine::class,
+							Application::class,
+						]
+					]),
+				],
+			])
 		];
 	}
 
