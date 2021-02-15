987bc63715b130007514a1bdb28b58a2ab4357b1
Salim Djerbouh <caddydz4@gmail.com>
2021-02-15 12:36:25 +0100

    Specify fpm service to reload
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ php artisan l5-swagger:generate
 php artisan optimize
 
 ( flock -w 10 9 || exit 1
-    echo 'Restarting FPM...'; sudo -S service $FORGE_PHP_FPM reload ) 9>/tmp/fpmlock
+    echo 'Restarting FPM...'; sudo -S service php7.4-fpm reload ) 9>/tmp/fpmlock
