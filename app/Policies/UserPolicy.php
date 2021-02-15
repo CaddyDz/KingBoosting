b1bc7a442f67483767751e660cd355ed4022298d
CaddyDz <CaddyDz@users.noreply.github.com>
2021-02-15 19:14:20 +0000

    Fix styling

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace App\Policies;
 
-use App\Models\Game;
-use App\Models\User;
+use App\Models\{Game, User};
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
