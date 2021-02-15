21c3e53891fc3335782ccbcbb262edcd07fe7e89
Salim Djerbouh <caddydz4@gmail.com>
2021-02-15 20:10:43 +0100

    Removed unused function
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,16 +10,6 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
 	/**
-	 * Register any application services.
-	 *
-	 * @return void
-	 */
-	public function register(): void
-	{
-		//
-	}
-
-	/**
 	 * Bootstrap any application services.
 	 *
 	 * @return void
