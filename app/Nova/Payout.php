3a2980668091427ce77ad5fd3fba38878b0cb444
Salim Djerbouh <caddydz4@gmail.com>
2021-02-16 15:09:10 +0100

    Formatting
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- app/Nova/Payout.php
+++ app/Nova/Payout.php
@@ -28,8 +28,8 @@ class Payout extends Order
 	 *
 	 * This query determines which instances of the model may be attached to other resources.
 	 *
-	 * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
-	 * @param  \Illuminate\Database\Eloquent\Builder  $query
+	 * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+	 * @param \Illuminate\Database\Eloquent\Builder $query
 	 * @return \Illuminate\Database\Eloquent\Builder
 	 */
 	public static function relatableQuery(NovaRequest $request, $query)
