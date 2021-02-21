a7c3a174130080aa15a82e67346dfd5b8153e383
Salim Djerbouh <caddydz4@gmail.com>
2021-02-21 22:08:29 +0100

    Link back to frontend
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/views/vendor/nova/layout.blade.php
+++ resources/views/vendor/nova/layout.blade.php
@@ -28,7 +28,7 @@
 		<div v-cloak class="flex min-h-screen">
 			<!-- Sidebar -->
 			<collapsible-sidebar class="min-h-screen flex-none pt-header min-h-screen w-sidebar bg-grad-sidebar px-6" persist>
-				<a href="{{ config('sanctum.stateful')[0] }}">
+				<a href="{{ \Laravel\Nova\Nova::path() }}">
 					<div class="absolute pin-t pin-l pin-r bg-logo flex items-center w-sidebar h-header px-6 text-white">
 						@include('nova::partials.logo')
 					</div>
