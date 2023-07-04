318aeb0538cc528c63dff67dc443143574dfc21a
Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>
2023-07-04 12:45:07 +0100

    Cleanup (#464)
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- frontend/components/layout/AppBarComponent.vue
+++ frontend/components/layout/AppBarComponent.vue
@@ -38,8 +38,6 @@ export default {
 		links: [
 			{ title: "Elo Boosting", url: "division-boosting" },
 			{ title: "Coaching", url: "coaching" },
-			{ title: "Account Market", url: "lol-accounts" },
-			{ title: "More", url: "" },
 		],
 		dialog: false,
 		register: false,
