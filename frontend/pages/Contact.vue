98fb650be14a7fb73403d3e444299ed22a1e685a
Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>
2023-07-04 16:16:45 +0100

    Cleanup contact page (#465)
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- frontend/pages/Contact.vue
+++ frontend/pages/Contact.vue
@@ -1,53 +1,57 @@
 <template>
-	<div class="contact-page">
-		<p class="d-flex justify-center font-weight-medium">Help Center</p>
-		<p class="d-flex justify-center font-weight-medium">Contact Us</p>
-		<p class="d-flex justify-center font-weight-medium">You can choose from a range of contact types. All you have to do is to find the one that suits you the best and we are ready to answer any of your questions as soon as possible.</p>
+	<div class="ma-16">
+		<h1 class="d-flex justify-center">Contact us</h1>
+		<div class="d-flex justify-center mt-5 text-center">
+			<p style="max-width: 700px;">
+				You can choose from a range of contact types. <br>
+				All you have to do is to find the one that suits you the best
+				and we are ready to answer any of your questions as soon as possible.
+			</p>
+		</div>
 		<v-container>
 			<v-row>
 				<v-col>
-					<v-card>
-						<v-card-title>Skype</v-card-title>
-						<v-card-text>
-							<a href="https://facebook.com/kingboosting">KingBoosting</a>
-							<v-icon x-large>mdi-skype</v-icon>
-						</v-card-text>
-					</v-card>
+					<a href="https://facebook.com/kingboosting">
+						<v-card>
+							<v-card-title>Facebook</v-card-title>
+							<v-card-text>
+								KingBoosting
+								<v-icon x-large>mdi-facebook</v-icon>
+							</v-card-text>
+						</v-card>
+					</a>
 				</v-col>
 				<v-col>
-					<v-card>
-						<v-card-title>Facebook</v-card-title>
-						<v-card-text>
-							<a href="https://facebook.com/kingboosting">KingBoosting</a>
-							<v-icon x-large>mdi-facebook</v-icon>
-						</v-card-text>
-					</v-card>
+					<a href="https://twitter.com/kingboosting">
+						<v-card>
+							<v-card-title>Twitter</v-card-title>
+							<v-card-text>
+								KingBoosting
+								<v-icon x-large>mdi-twitter</v-icon>
+							</v-card-text>
+						</v-card>
+					</a>
 				</v-col>
 				<v-col>
-					<v-card>
-						<v-card-title>Twitter</v-card-title>
-						<v-card-text>
-							<a href="https://twitter.com/kingboosting">KingBoosting</a>
-							<v-icon x-large>mdi-twitter</v-icon>
-						</v-card-text>
-					</v-card>
+					<a href="https://www.instagram.com/kingboosting_official/">
+						<v-card>
+							<v-card-title>Instagram</v-card-title>
+							<v-card-text>
+								KingBoosting
+								<v-icon x-large>mdi-instagram</v-icon>
+							</v-card-text>
+						</v-card>
+					</a>
 				</v-col>
-			</v-row>
-			<v-row>
-				<v-card width="100%">
-					<v-card-title>Instagram</v-card-title>
-					<v-card-text>
-						<a href="https://www.instagram.com/kingboosting_official/">KingBoosting</a>
-						<v-icon x-large>mdi-instagram</v-icon>
-					</v-card-text>
-				</v-card>
 			</v-row>
 			<v-row>
 				<v-col>
 					<v-form ref="form" v-model="valid">
-						<v-select v-model="category" name="Category" label="Category" :rules="[(v) => !!v || 'Category is required']" :items="categories"></v-select>
+						<v-select v-model="category" name="Category" label="Category"
+							:rules="[(v) => !!v || 'Category is required']" :items="categories"></v-select>
 						<v-text-field name="name" label="Name" :rules="nameRules"></v-text-field>
-						<v-text-field name="email" label="E-mail address" :rules="[(v) => /.+@.+\..+/.test(v) || 'E-mail must be valid']"></v-text-field>
+						<v-text-field name="email" label="E-mail address"
+							:rules="[(v) => /.+@.+\..+/.test(v) || 'E-mail must be valid']"></v-text-field>
 						<v-textarea outlined name="message" placeholder="Your Message"></v-textarea>
 						<v-btn :disabled="!valid" color="primary" class="mr-4" @click="submit">Send Message</v-btn>
 					</v-form>
@@ -111,9 +115,3 @@ export default {
 	},
 };
 </script>
-
-<style scoped>
-.contact-page {
-	padding: 20px;
-}
-</style>
