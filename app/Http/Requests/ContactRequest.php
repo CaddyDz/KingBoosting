0c073324bf1e8b2db36a573bb1df87de3e49f256
CaddyDz <CaddyDz@users.noreply.github.com>
2023-07-06 09:00:36 +0000

    Fix styling

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
 
 class ContactRequest extends FormRequest
 {
